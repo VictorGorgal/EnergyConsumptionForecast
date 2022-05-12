cc74a6766b342246ce8bf481331a94fd7c689c93
VictorGorgal <94933775+VictorGorgal@users.noreply.github.com>
2022-05-12 12:16:39 -0300

    Add files via upload

---
+++ Neural-net/nn.py
@@ -0,0 +1,44 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+import read_dataset as rds
+from keras.preprocessing.sequence import TimeseriesGenerator
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
+
+
+EPOCHS = 10
+BATCH_SIZE = 64
+
+POINTS = 48  # entire project revolves on just 48 points
+
+
+# using more days seemed to have a negative impact
+df, dates = rds.read_dataset(points=POINTS, days=500)
+dataset = rds.preprocess(df)
+
+train, test = rds.split_data(dataset)
+train = train.flatten()
+train = train.reshape((len(train), 1))
+
+train_generator = TimeseriesGenerator(train, train,
+                                      length=POINTS,
+                                      sampling_rate=1, stride=1,
+                                      batch_size=BATCH_SIZE)
+
+neurons = 144
+
+model = Sequential()
+model.add(LSTM(neurons, input_shape=(POINTS, 1), return_sequences=True))
+model.add(LSTM(1, input_shape=(neurons,), return_sequences=True))
+model.add(LSTM(neurons, input_shape=(1,)))
+model.add(Dense(1))
+model.compile(loss='mse', optimizer='adam')
+
+history = model.fit(train_generator, epochs=EPOCHS)
+
+model.save('main_model.h5')
+
+plt.plot(history.history['loss'])
+plt.title('model loss')
+plt.show()
---
+++ Neural-net/predictions.py
@@ -0,0 +1,61 @@
+import os
+import random
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+from keras.models import load_model
+import read_dataset
+import numpy as np
+import matplotlib.pyplot as plt
+
+POINTS = 48
+BATCH = 32
+
+
+def add(array, element):
+    new = np.zeros(array.shape)
+    new[0, :-1, 0] = array[0, 1:, 0]
+    new[0, -1, 0] = element
+    return new
+
+
+def predict(model, data, future=144):
+    y_hat = []
+    for _ in range(future):
+        pred = model.predict(data)[0, 0]
+
+        y_hat.append(pred)
+        data = add(data, pred)
+
+    return y_hat
+
+
+model = load_model('main_model.h5')
+
+
+df, _ = read_dataset.read_dataset(points=POINTS)
+dataset = read_dataset.preprocess(df)
+train, test = read_dataset.split_data(dataset, percentage=5)
+
+ds = test
+
+print(f'dataset length: {len(ds)}')
+
+while True:
+    i = random.randint(0, len(ds) - 1)
+    print(f'day #{i}')
+    X = ds[i]
+    X = X.reshape((1, len(X), 1))
+    y = ds[i + 1]
+
+    y_hat = predict(model, X, future=POINTS)
+
+    x = np.linspace(0, 24, POINTS)
+    plt.title('Main Model')
+    plt.plot(x, X.flatten() * 12, label='base')
+    x = np.linspace(24, 48, POINTS)
+    plt.plot(x, y.flatten() * 12, label='actual')
+    plt.plot(x, np.array(y_hat) * 12, linestyle=':', label='predicted')
+    plt.xlabel('Time')
+    plt.ylabel('kW')
+    plt.legend(loc='upper left')
+
+    plt.show()
